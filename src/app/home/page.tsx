f91b125abf77b2d7618412536db12f1663319f89
Kelvi <kelvingremista2015@hotmail.com>
2024-01-25 23:29:34 -0300

    :construction: page home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-import Banner from "../../../public/banner-02.png";
 export default function Home() {
   return (
-    <div className="bg-[#2A2F38] h-full">
-      <Image src={Banner} alt="Banner" className="z-0"/>
-    </div>
+    <section className="bg-[#2A2F38] w-full h-[25rem] image-background-div flex justify-center">
+      <main className="flex flex-col mt-24 items-center gap-8">
+      <div className="max-w-[50rem]">
+        <h1 className="text-4xl text-[#fff] leading-10 font-semibold text-center">Pesquise pelo ativo desejado para ter acesso a cotação</h1>
+      </div>
+      <div>
+        <input type="text" className="text-lg text-[#666] p-[0.8px] leading-[3.75rem] rounded-[1.87rem] border outline-none border-[#88601a]"/>
+      </div>
+      </main>
+    </section>
   );
 }
